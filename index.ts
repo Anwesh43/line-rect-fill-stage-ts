1dd3f9ac60a90bfd595ad9e175e2b1dcad345497
Anwesh43 <anweshthecool0@gmail.com>
2020-10-04 20:58:08 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import { min } from "lodash"
+
 const w : number = window.innerWidth 
 const h : number = window.innerHeight 
 const strokeFactor : number = 90
@@ -13,3 +15,18 @@ const colors : Array<string> = [
 ]
 const parts : number = 3 
 const scGap : number = 0.02 / parts 
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
